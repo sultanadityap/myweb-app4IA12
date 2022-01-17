b3ea25112122a7e380e47ade615f6d7b33557a77
sultanadityap <sultanaditya9561@gmail.com>
2022-01-17 14:33:59 +0700

    push all file to github

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <link href=" {{ mix('css/app.css') }}" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Setup Laravel Bootstrap Succesfully <i class="fas fa-ad"></i></h1>
+        <script src="{{ mix('js/app.js') }}"> }}"><"/script>
+    </body>
+</html>
